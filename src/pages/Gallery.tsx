0b35c23fc6b48f02519d7e024cd3e1b68a964873
Jason Gonsalves <74058020+Jason3105@users.noreply.github.com>
2025-07-14 20:01:00 +0530

    Update Gallery.tsx

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -7,6 +7,7 @@ import { X, ChevronLeft, ChevronRight, ArrowLeft } from 'lucide-react';
 import { useSearchParams } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { Helmet } from "react-helmet";
 
 const Gallery = () => {
   const [selectedCategory, setSelectedCategory] = useState('All');
